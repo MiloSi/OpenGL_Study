cb6fb301629453a73b46f16b08eb261056b76e83
C <aquarius456@nate.com>
2017-06-04 02:08:19 +0900

    Add files via upload

---
+++ first_3d/first_3d.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <gl\glut.h>
+
+void init();
+void display();
+void mouseHandler(int btn, int state, int x, int y);
+
+GLfloat vertices[][3] = { { -1.0,-1.0,-1.0 },{ 1.0,-1.0,-1.0 },{ 1.0,1.0,-1.0 },{ -1.0,1.0,-1.0 },
+{ -1.0,-1.0, 1.0 },{ 1.0,-1.0, 1.0 },{ 1.0,1.0, 1.0 },{ -1.0,1.0, 1.0 } };
+GLfloat colors[][3] = { { 0.0, 0.0, 0.0 },{ 1.0, 0.0, 0.0 },{ 1.0,1.0, 0.0 },{ 0.0,1.0, 0.0 },
+{ 0.0, 0.0, 1.0 },{ 1.0, 0.0, 1.0 },{ 1.0,1.0, 1.0 },{ 0.0,1.0, 1.0 } };
+
+
+void mouseHandler(int btn, int state, int x, int y)
+{
+	if (btn == GLUT_LEFT_BUTTON && state == GLUT_DOWN);
+	if (btn == GLUT_RIGHT_BUTTON && state == GLUT_DOWN);
+
+}
+
+void polygon(int a, int b, int c, int d)
+{
+	glBegin(GL_POLYGON);
+	glColor3fv(colors[a]);
+	glVertex3fv(vertices[a]);
+	glColor3fv(colors[b]);
+	glVertex3fv(vertices[b]);
+	glColor3fv(colors[c]);
+	glVertex3fv(vertices[c]);
+	glColor3fv(colors[d]);
+	glVertex3fv(vertices[d]);
+	glEnd();
+}
+
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	polygon(0, 3, 2, 1);
+	polygon(2, 3, 7, 6);
+	polygon(0, 4, 7, 3);
+	polygon(1, 2, 6, 5);
+	polygon(4, 5, 6, 7);
+	polygon(0, 1, 5, 4);
+	glFlush();
+}
+
+void init() {
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glOrtho(-2.0, 2.0, -2.0, 2.0, -10, 10);
+	glEnable(GL_DEPTH_TEST);
+}
+
+
+int main(int argc, char * argv[])
+{
+
+	//in glut version, it should 
+	glutInit(&argc, (char**)argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(500, 500);
+
+
+	glutCreateWindow("First 3D");
+	glutDisplayFunc(display);
+
+	glutMouseFunc(mouseHandler);
+	init();
+	glutMainLoop();
+
+	return 0;
+}
