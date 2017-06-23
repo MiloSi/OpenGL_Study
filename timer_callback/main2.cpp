21aa4a93624ac871e1a4b2afb12eaf85b137df64
C <aquarius456@nate.com>
2017-06-23 23:08:57 +0900

    v2

---
+++ timer_callback/main2.cpp
@@ -0,0 +1,108 @@
+#include <gl\glut.h>
+#include <gl\GL.h>
+#include <gl\GLU.h>
+#include <time.h>
+
+
+const unsigned char PALETTE[16][3] = {
+	{ 255, 255, 255 },
+	{ 0, 255, 255 },
+	{ 255, 0, 255 },
+	{ 0,0, 255 },
+	{192, 192, 192 },
+	{ 128, 128, 128 },
+	{0, 128, 128 },
+	{ 128, 0, 128 },
+	{ 0, 0, 128 },
+	{ 255, 255, 0 },
+	{ 0, 255,0  },
+	{ 128, 128, 0 },
+	{ 0, 128,  0},
+	{ 255, 0, 0 },
+	{128, 0,  0 },
+	{ 0, 0,  0 }
+};
+
+GLfloat delta = 0.0f;
+GLint index = 0;
+GLfloat r = 0.0f;
+GLfloat g = 0.0f;
+GLfloat b = 0.0f;
+
+
+void myDisplay() {
+
+	// 255를 1로 노말리제이션
+	r = PALETTE[index][0] / 255.0f;
+	g = PALETTE[index][1] / 255.0f;
+	b = PALETTE[index][2] / 255.0f;
+
+	glColor3f(r, g, b);
+
+	glBegin(GL_LINES);
+	glVertex3f(-1.0f + delta, 1.0f, 0.f);
+	glVertex3f(1.0f - delta, -1.0f, 0.f);
+	glVertex3f(-1.0 , -1.0 + delta, 1.0);
+	glVertex3f(1.0 , 1.0 - delta, 1.0);
+	glEnd();
+	glutSwapBuffers();
+
+
+
+
+
+
+	/*
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POLYGON);
+	glColor3f(0.0f, 0.5f, 0.8f);
+	glVertex3f(-1.0f + delta, -0.5f, 0.0f);
+	glVertex3f(0.0f + delta, -0.5f, 0.0f);
+	glVertex3f(0.0f + delta, 0.5f, 0.0f);
+	glVertex3f(-1.0f + delta, 0.5f, 0.0f);
+	glEnd();
+	//플러쉬 말고 버퍼 메인에서 더블을 사용하니까..
+	glutSwapBuffers();
+
+	*/
+}
+void myTimer(int value) {
+
+	if (delta < 2.0f)
+		delta = delta + 0.01;
+	else
+	{
+		delta = 0;
+		if (++index >= 15) {
+			index = 0;
+			glClear(GL_COLOR_BUFFER_BIT);
+		}
+	}
+	glutPostRedisplay();
+	glutTimerFunc(10, myTimer, 1);
+}
+
+void init() {
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1.0, 1.0, 1.0, 1.0, -1.0, 1.0);
+
+}
+
+int main(int argc, char ** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	glutInitWindowSize(300, 300);
+	glutInitWindowPosition(0, 0);
+
+	glutCreateWindow("OpenGL Drawing Example");
+
+	init();
+	glutDisplayFunc(myDisplay);
+	glutTimerFunc(10, myTimer, 1);
+	
+	
+	glutMainLoop();
+	return 0;
+}
