f83396206b7ae252569168a161fddc6ea440306e
C <aquarius456@nate.com>
2017-06-24 12:31:20 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,26 @@
-#pragma once
-#include "projection.h"
+#include "teapot.h"
+#include <Windows.h>
 
+/*
+int main(int argc, char** argv)
+{
+	TEAPOT teapot(argc, argv);
 
-int main(int argc, char** argv) {
-	glutInit(&argc, (char**)argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(500, 500);
-
-
-	windowID = glutCreateWindow("ROTATE CUBE");
-
-	glutIdleFunc(reDisplay);
-	glutMouseFunc(mouseHandler);
-	glutMotionFunc(motionHandler);
-	glutKeyboardFunc(keyboardHandler);
-
-	
-	glEnableClientState(GL_COLOR_ARRAY);
-	glEnableClientState(GL_VERTEX_ARRAY);
-
-	glVertexPointer(3, GL_FLOAT, 0, vertices);
-	glColorPointer(3, GL_FLOAT, 0, colors);
-
-	glutDisplayFunc(display);
-
-	init();
-	glutMainLoop();
+	teapot.setWindowPosition(0, 0);
+	teapot.setWindowSize(800, 800);
+	teapot.show();
 
 	return 0;
+
+}*/
+int APIENTRY WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPSTR lpzCmdParam, int nCmdeShow) {
+	TEAPOT *teapot = new TEAPOT();
+
+	teapot->setWindowPosition(0, 0);
+	teapot->setWindowSize(800, 800);
+	teapot->show();
+
+	return 0;
+
+	delete teapot;
 }
