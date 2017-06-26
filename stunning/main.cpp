ff88cedc7da3a73365500566c3b44da72cfa7f18
C <aquarius456@nate.com>
2017-06-26 22:55:20 +0900

    refactoring

---
+++ stunning/main.cpp
@@ -0,0 +1,12 @@
+#include <Windows.h>
+#include "solarsystem.h"
+
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
+	SolarSystem* solar =  new SolarSystem();
+	solar->showWindow();
+
+
+	delete solar;
+	return 0;
+}
